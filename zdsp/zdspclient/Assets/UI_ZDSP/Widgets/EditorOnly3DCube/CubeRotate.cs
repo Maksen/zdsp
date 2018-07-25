d3b64531d34ad408b959b852500bcc1a8f996cf7
junming <guan.junming85@gmail.com>
2018-07-25 11:05:27 +0800

    UI part3

---
+++ zdsp/zdspclient/Assets/UI_ZDSP/Widgets/EditorOnly3DCube/CubeRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CubeRotate : MonoBehaviour {
+    public float turnSpeed = 50f;
+    public bool Right;
+
+    // Update is called once per frame
+    void Update () {
+        if (!Right)
+        {
+            transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime);
+        } else {
+            transform.Rotate(Vector3.right * turnSpeed * Time.deltaTime);
+        }
+        
+    }
+}

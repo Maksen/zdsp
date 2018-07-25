51d14668912a15fa6af9f2abafe3723c9f372e06
junming <guan.junming85@gmail.com>
2018-07-25 12:16:47 +0800

    external

---
+++ zdsp/zdspclient/Assets/External/FancyScrollView/Examples/03_InfiniteScroll/Example03Scene.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace FancyScrollView
+{
+    public class Example03Scene : MonoBehaviour
+    {
+        [SerializeField]
+        Example03ScrollView scrollView;
+
+        [SerializeField]
+        int CellCount = 20;
+
+        void Start()
+        {
+            var cellData = Enumerable.Range(0, CellCount)
+                .Select(i => new Example03CellDto { Message = "Cell " + i })
+                .ToList();
+
+            scrollView.UpdateData(cellData);
+        }
+    }
+}

e6792b0580f7d343bd3a7754c22a6fe68d8ddf2a
junming <guan.junming85@gmail.com>
2018-11-07 16:03:41 +0800

    svn

--- zdsp/zdspclient/Assets/UI_ZDSP/Scenes/aaa_HUD/ParticleLabel/ParticleLabel.cs
+++ zdsp/zdspclient/Assets/UI_ZDSP/Scenes/aaa_HUD/ParticleLabel/ParticleLabel.cs
@@ -5,11 +5,34 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.UI;
 
+public class TransformCopy
+{
+    public Vector3 position;
+    public Quaternion rotation;
+    public Vector3 scale;
+
+    public TransformCopy(Transform t)
+    {
+        position = new Vector3(t.position.x, t.position.y, t.position.z);
+        rotation = new Quaternion(t.rotation.x, t.rotation.y, t.rotation.z, t.rotation.w);
+        scale = new Vector3(t.localScale.x, t.localScale.y, t.localScale.z);
+    }
+
+    public void Set(Transform t)
+    {
+        t.position = position;
+        t.rotation = rotation;
+        t.localScale = scale;
+    }
+}
+
 public class ParticleLabel : MonoBehaviour {
 
     public Camera TextCamera;
     public ParticleSystem LabelParticle;
-    public List<Text> RenderTexts;
+    public Text RenderText;
+    public Transform TextParent;
+    List<Text> RenderTexts;
 
     CommandBuffer commandBuffer;
     RenderTexture debugRT;
@@ -21,27 +44,101 @@ public class ParticleLabel : MonoBehaviour {
 
     List<ParticleSystem> particlesystems;
 
-    int size = 8;
+    int size = 16;
     int index = 0;
     // Use this for initialization
-    void Start ()
+    void Start()
     {
+    }
+
+    bool inited = false;
+    void init()
+    {        
+        Reset();
+        gameObject.SetActive(true);
+
         particlesystems = new List<ParticleSystem>();
+        var tsa = LabelParticle.textureSheetAnimation;
+        tsa.numTilesY = size;
         for (int i = 0; i < size; ++i)
         {
             var system = Instantiate(LabelParticle, particleparent);
+            system.gameObject.SetActive(true);            
             particlesystems.Add(system);
         }
+        LabelParticle.gameObject.SetActive(false);
+
+        RenderTexts = new List<Text>();
+        for (int i = 0; i < size; ++i)
+        {
+            var text = Instantiate(RenderText, TextParent);
+            text.gameObject.SetActive(true);
+            text.transform.localPosition = RenderText.transform.localPosition + new Vector3(0.0f, -RenderText.rectTransform.rect.height * i, 0.0f);
+            RenderTexts.Add(text);
+        }
+        RenderText.gameObject.SetActive(false);
+
+        inited = true;
     }
-    
-    void Emit(string text)
+
+    void SetLabelPositions()
     {
+        int i = 0;
+        foreach(var text in RenderTexts)
+        {
+            text.gameObject.SetActive(true);
+            text.transform.localPosition = RenderText.transform.localPosition + new Vector3(0.0f, -RenderText.rectTransform.rect.height * i, 0.0f);
+            ++i;
+        }
+    }
+
+    private void Reset()
+    {
+        if(particlesystems != null)
+        foreach (var system in particlesystems)
+        {
+            Destroy(system);
+        }
+        particlesystems = null;
+
+        if(RenderTexts != null)
+        foreach (var text in RenderTexts)
+        {
+            Destroy(text);
+        }
+        RenderTexts = null;
+
+        inited = false;
+    }
+
+    class EmitCommand
+    {
+        public string text = "";
+        public TransformCopy transform;  
+    }
+    List<EmitCommand> command_buffer = new List<EmitCommand>();
+    public void Emit(string text)
+    {
+        command_buffer.Add(new EmitCommand { text = text, transform = new TransformCopy(gameObject.transform) });
+
+        if (inited == false)
+            init();
+    }
+
+    public float RandomDirectionMagnitude = 15.0f;
+    void Emit(EmitCommand cmd)
+    {
+        var text = cmd.text;
+        cmd.transform.Set(gameObject.transform);
+
+        SetLabelPositions();
         RenderTexts[index].text = text;
 
-        debugRT = new RenderTexture(TextCamera.pixelWidth, TextCamera.pixelHeight, 16);
-        debugRT.Create();
-
+        //debugRT = new RenderTexture(TextCamera.pixelWidth, TextCamera.pixelHeight, 16);
+        //debugRT.Create();
+        
         var currentsystem = particlesystems[index];
+        currentsystem.gameObject.transform.rotation = Quaternion.identity * Quaternion.Euler(Random.Range(-RandomDirectionMagnitude, RandomDirectionMagnitude), Random.Range(-RandomDirectionMagnitude, RandomDirectionMagnitude), Random.Range(-RandomDirectionMagnitude, RandomDirectionMagnitude));
 
         var ts = currentsystem.textureSheetAnimation;
         ts.enabled = true;
@@ -50,6 +147,8 @@ public class ParticleLabel : MonoBehaviour {
         var curve = new ParticleSystem.MinMaxCurve(delta);
         ts.frameOverTime = curve;
 
+        currentsystem.randomSeed = (uint)(Time.realtimeSinceStartup * 1000);
+        currentsystem.useAutoRandomSeed = false;
         currentsystem.Emit(1);
         
         index = (index + 1) % size;
@@ -66,6 +165,13 @@ public class ParticleLabel : MonoBehaviour {
                 Emit(Random.Range(0, 1000).ToString());
             else
                 Emit(EmittedText);
-        }		
+        }
+
+        if (command_buffer.Count > 0)
+        {
+            var emitted = command_buffer[0];
+            Emit(emitted);
+            command_buffer.Remove(emitted);
+        }
 	}
 }

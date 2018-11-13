f324e6d1ef65c5c566602c81bb328d920dfabfec
Guan Junming <guan.junming85@gmail.com>
2018-11-13 16:39:07 +0800

    temp

---
+++ zdsp/zdspclient/Assets/scripts/GUI/Achievement/UI_AchievementMessage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI_AchievementMessage : MonoBehaviour
+{
+    [SerializeField] Text messageText;
+
+    private Queue<string> buffer = new Queue<string>();
+
+    public void ShowMessage(string message)
+    {
+        buffer.Enqueue(message);
+        if (!gameObject.activeInHierarchy)
+            gameObject.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        if (buffer.Count > 0)
+            messageText.text = buffer.Dequeue();
+    }
+
+    // Called by animation on last frame
+    public void OnFinished()
+    {
+        if (buffer.Count > 0)
+            gameObject.SetActive(true);
+    }
+}

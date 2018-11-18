2ea0da54342b640c814f0a2c6cae293c10e7ce41
Guan Junming <guan.junming85@gmail.com>
2018-11-18 20:25:56 +0800

    new

---
+++ zdsp/zdspclient/Assets/scripts/GUI/Achievement/AchievementData/Achievement_ItemOriginData.cs
@@ -0,0 +1,123 @@
+﻿using Kopio.JsonContracts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zealot.Bot;
+using Zealot.Client.Entities;
+using Zealot.Common;
+using Zealot.Entities;
+using Zealot.Repository;
+
+public class Achievement_ItemOriginData : MonoBehaviour
+{
+    [SerializeField] Image iconImage;
+    [SerializeField] Text nameText;
+    [SerializeField] Text levelText;
+    [SerializeField] Text mapNameText;
+    [SerializeField] Button button;
+
+    public void Init(ItemOriginType originType, string param)
+    {
+        switch (originType)
+        {
+            case ItemOriginType.Monster:
+                CombatNPCJson combatNPCJson = CombatNPCRepo.GetNPCByArchetype(param);
+                if (combatNPCJson != null)
+                {
+                    iconImage.sprite = ClientUtils.LoadIcon(combatNPCJson.portraitpath);
+                    nameText.text = combatNPCJson.localizedname;
+                    levelText.text = GUILocalizationRepo.GetLocalizedString("com_level") + combatNPCJson.level;
+                    string destLevel = "";
+                    Vector3 destPos = Vector3.zero;
+                    if (NPCPosMap.FindNearestMonster(combatNPCJson.archetype, ClientUtils.GetCurrentLevelName(), GameInfo.gLocalPlayer.Position, ref destLevel, ref destPos))
+                    {
+                        var levelData = LevelRepo.GetInfoByName(destLevel);
+                        mapNameText.text = levelData.localizedname;
+                        button.onClick.AddListener(() => PathFindToTarget(destLevel, destPos, ReachTargetAction.None));
+                    }
+                    else
+                    {
+                        mapNameText.text = "";
+                        button.gameObject.SetActive(false);
+                    }                       
+                }
+                break;
+            case ItemOriginType.Item:
+                int itemId;
+                if (int.TryParse(param, out itemId))
+                {
+                    IInventoryItem item = GameRepo.ItemFactory.GetInventoryItem(itemId);
+                    if (item != null)
+                    {
+                        iconImage.sprite = ClientUtils.LoadIcon(item.JsonObject.iconspritepath);
+                        nameText.text = item.JsonObject.localizedname;
+                        levelText.text = "";
+                        mapNameText.text = "";
+                        button.gameObject.SetActive(false);
+                    }
+                }
+                break;
+            case ItemOriginType.NPC:
+                StaticNPCJson staticNPCJson = StaticNPCRepo.GetNPCByArchetype(param);
+                if (staticNPCJson != null)
+                {
+                    iconImage.sprite = ClientUtils.LoadIcon(staticNPCJson.portraitpath);
+                    nameText.text = staticNPCJson.localizedname;
+                    levelText.text = "";
+                    string destLevel = "";
+                    Vector3 destPos = Vector3.zero;
+                    if (NPCPosMap.FindNearestStaticNPC(staticNPCJson.archetype, ClientUtils.GetCurrentLevelName(), GameInfo.gLocalPlayer.Position, ref destLevel, ref destPos))
+                    {
+                        var levelData = LevelRepo.GetInfoByName(destLevel);
+                        mapNameText.text = levelData.localizedname;
+                        button.onClick.AddListener(() => PathFindToTarget(destLevel, destPos, ReachTargetAction.NPC_Interact, staticNPCJson.id));
+                    }
+                    else
+                    {
+                        mapNameText.text = "";
+                        button.gameObject.SetActive(false);
+                    }
+                }
+                break;
+            case ItemOriginType.UI:
+                break;
+            case ItemOriginType.Auction:
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void PathFindToTarget(string targetLevel, Vector3 targetPos, ReachTargetAction targetAction, int targetId = -1)
+    {
+        UIManager.CloseAllDialogs();
+        UIManager.CloseAllWindows();
+
+        PlayerGhost player = GameInfo.gLocalPlayer;
+        string currentLevel = ClientUtils.GetCurrentLevelName();
+
+        if (currentLevel == targetLevel)
+        {
+            if (targetAction == ReachTargetAction.NPC_Interact)
+                player.ProceedToTarget(targetPos, targetId, CallBackAction.Interact);
+            else
+                player.PathFindToTarget(targetPos, -1, 0, false, false, null);
+        }
+        else
+        {
+            bool foundtarget;
+            BotController.TheDijkstra.DoRouter(currentLevel, targetLevel, out foundtarget);
+            if (foundtarget)
+            {
+                BotController.DestLevel = targetLevel;
+                BotController.DestMapPos = targetPos;
+                BotController.DestAction = targetAction;
+                BotController.DestArchtypeID = targetId;
+                GameInfo.gLocalPlayer.Bot.SeekingWithRouter();
+            }
+            else
+                UIManager.ShowSystemMessage(GUILocalizationRepo.GetLocalizedSysMsgByName("sys_CannotFindTarget"));                           
+        }
+    }
+}
---
+++ zdsp/zdspclient/Assets/scripts/GUI/Achievement/UI_Achievement_CollectionOriginDialog.cs
@@ -0,0 +1,37 @@
+﻿using Kopio.JsonContracts;
+using UnityEngine;
+using Zealot.Repository;
+
+public class UI_Achievement_CollectionOriginDialog : BaseWindowBehaviour
+{
+    [SerializeField] Transform dataParent;
+    [SerializeField] GameObject dataPrefab;
+
+    public void Init(string originStr)
+    {
+        int originId;
+        ItemOriginJson itemOriginJson;
+        string[] origins = originStr.Split(';');
+        for (int i = 0; i < origins.Length; ++i)
+        {
+            if (int.TryParse(origins[i], out originId) && GameRepo.ItemFactory.ItemOriginTable.TryGetValue(originId, out itemOriginJson))
+                InitOriginData(itemOriginJson);
+        }
+    }
+
+    private void InitOriginData(ItemOriginJson json)
+    {
+        string[] paramArr = json.param.Split(';');
+        for (int i = 0; i < paramArr.Length; i++)
+        {
+            GameObject go = ClientUtils.CreateChild(dataParent, dataPrefab);
+            go.GetComponent<Achievement_ItemOriginData>().Init(json.origintype, paramArr[i]);
+        }
+    }
+
+    public override void OnCloseWindow()
+    {
+        base.OnCloseWindow();
+        ClientUtils.DestroyChildren(dataParent);
+    }
+}

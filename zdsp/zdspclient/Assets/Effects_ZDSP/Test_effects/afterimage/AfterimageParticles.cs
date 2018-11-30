3b37435cee96789c7003e33f54a7e955b9e1d60a
junming <guan.junming85@gmail.com>
2018-11-30 14:36:53 +0800

    svn

---
+++ zdsp/zdspclient/Assets/Effects_ZDSP/Test_effects/afterimage/AfterimageParticles.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PooledEffects : MonoBehaviour
+{
+    public abstract void Restart();
+}
+
+public class AfterimageParticles : PooledEffects
+{
+    public SkinnedMeshRenderer target;
+
+    public float frequency = 2.0f;
+    public float duration = 0.0f;
+    public float delay = 0.0f;
+
+    ParticleSystem[] particlesystems;
+    ParticleSystemRenderer[] particlerenderers;
+
+    // Use this for initialization
+    void Start()
+    {
+        init();
+    }
+
+    private void OnEnable()
+    {
+        init();
+    }
+
+    public override void Restart()
+    {
+        init();
+    }
+
+    void init()
+    {
+        durationclock = 0.0f;
+        particlerenderers = GetComponentsInChildren<ParticleSystemRenderer>();
+        particlesystems = GetComponentsInChildren<ParticleSystem>();
+    }
+
+    float clock = 0.0f;
+    int index = 0;
+    bool running = true;
+    float durationclock = 0.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateClocks();
+
+        if (running)
+        {
+            float period = 1.0f / frequency;
+            clock += Time.deltaTime;
+
+            if (clock >= period)
+            {
+                Mesh mesh = new Mesh();
+                target.BakeMesh(mesh);
+
+                particlerenderers[index].mesh = mesh;
+                particlesystems[index].Emit(1);
+
+                clock = 0.0f;
+                index = (index + 1) % particlesystems.Length;
+            }            
+        }
+    }
+
+    void UpdateClocks()
+    {
+        if (durationclock < delay)
+        {
+            running = false;
+
+            durationclock += Time.deltaTime;            
+        }
+        else
+        {
+            if (duration == 0.0f)
+            {
+                running = true;
+                return;
+            }
+
+            running = durationclock < (delay + duration);
+
+            durationclock += Time.deltaTime;
+        }
+    }
+}
---
+++ zdsp/zdspclient/Assets/Effects_ZDSP/Test_effects/afterimage/MoveTester.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTester : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    public float speed = 1.0f;
+	void Update () {
+        transform.localPosition = transform.localPosition + new Vector3(0.0f, 0.0f, speed * Time.deltaTime);
+	}
+}
